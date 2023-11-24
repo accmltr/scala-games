4a9d738ad03fe8cddf7c86043bc9b7adf67902ee
Accumulator <johanreynekemail@gmail.com>
2023-11-25 02:11:46 +1000

    Refactoring and performance upgrades

--- src/main/scala/lib/instance_management/Instance.scala
+++ src/main/scala/lib/instance_management/Instance.scala
@@ -1,27 +1,27 @@
 package lib.instance_management
 
 final class Instance[T] private[instance_management] (
-    private var instance: Option[T],
+    private var _instance: Option[T],
     val id: Int,
     val manager: InstanceManager[T]
 ) {
   require(
-    instance.isDefined,
+    _instance.isDefined,
     "'instance' parameter may not be null"
   )
 
-  def isDestroyed: Boolean = instance.isEmpty
+  def isDestroyed: Boolean = _instance.isEmpty
 
   def destroy(): Unit = {
-    instance = None
-    manager.onDestroyed(this)
+    _instance = None
+    manager._onDestroyed(this)
   }
 }
 
 object Instance {
   // implicitly convert Reference[T] to T
   implicit def rti[T](ref: Instance[T]): T = {
-    ref.instance match {
+    ref._instance match {
       case Some(value) => value
       case None =>
         throw new IllegalStateException(
--- src/main/scala/lib/instance_management/InstanceManager.scala
+++ src/main/scala/lib/instance_management/InstanceManager.scala
@@ -1,35 +1,37 @@
 package lib.instance_management
 
-import scala.collection.mutable.Queue
+import scala.collection.immutable.Queue
 
 /** **Purpose:** Prevent memory leaks and track instances.
   */
 class InstanceManager[T](var historySize: Int = 10000) {
 
-  var instances: Map[Int, Instance[T]] = Map.empty
+  private var _instances: Map[Int, Instance[T]] = Map.empty
+  private var _nextId: Int = 0
+  private var _destroyedIds: Queue[Int] = Queue.empty
 
-  private var nextId: Int = 0
-  private var destroyedIds: Queue[Int] = Queue.empty
+  def instances: Vector[Instance[T]] = _instances.values.toVector
+  def destroyedIds: Vector[Int] = _destroyedIds.toVector
 
   def newId: Int = this.synchronized {
-    val id = nextId
-    nextId += 1
+    val id = _nextId
+    _nextId += 1
     id
   }
 
   def register(i: T): Instance[T] = this.synchronized {
     require(i != null, "'instance' parameter may not be null")
     val ref = Instance(Option(i), newId, this)
-    instances += ref.id -> ref
+    _instances += ref.id -> ref
     ref
   }
 
-  private[instance_management] def onDestroyed(instance: Instance[T]): Unit =
+  private[instance_management] def _onDestroyed(instance: Instance[T]): Unit =
     this.synchronized {
-      instances -= instance.id
-      if (destroyedIds.size >= historySize)
-        destroyedIds.dequeue()
-      destroyedIds.enqueue(instance.id)
+      _instances -= instance.id
+      if (_destroyedIds.size >= historySize)
+        _destroyedIds = _destroyedIds.dequeue._2
+      _destroyedIds = _destroyedIds.enqueue(instance.id)
     }
 
 }
