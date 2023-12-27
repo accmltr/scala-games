9294a702bb60e19e828f80c2912cd875474de5a9
accmltr <johanreynekemail@gmail.com>
2023-12-27 16:25:33 +1300

    Add translation constructor to Matrix3

--- src/main/scala/engine/math/Matrix3.scala
+++ src/main/scala/engine/math/Matrix3.scala
@@ -23,12 +23,31 @@ final case class Matrix3(
     Vector2(result.x / result.z, result.y / result.z)
   }
 
+  def *(other: Matrix3): Matrix3 = {
+    Matrix3(
+      // format: off
+      // column 0
+      (other.c0r0 * c0r0) + (other.c0r1 * c1r0) + (other.c0r2 * c2r0),
+      (other.c0r0 * c0r1) + (other.c0r1 * c1r1) + (other.c0r2 * c2r1),
+      (other.c0r0 * c0r2) + (other.c0r1 * c1r2) + (other.c0r2 * c2r2),
+      // column 1
+      (other.c1r0 * c0r0) + (other.c1r1 * c1r0) + (other.c1r2 * c2r0),
+      (other.c1r0 * c0r1) + (other.c1r1 * c1r1) + (other.c1r2 * c2r1),
+      (other.c1r0 * c0r2) + (other.c1r1 * c1r2) + (other.c1r2 * c2r2),
+      // column 2
+      (other.c2r0 * c0r0) + (other.c2r1 * c1r0) + (other.c2r2 * c2r0),
+      (other.c2r0 * c0r1) + (other.c2r1 * c1r1) + (other.c2r2 * c2r1),
+      (other.c2r0 * c0r2) + (other.c2r1 * c1r2) + (other.c2r2 * c2r2),
+      // format: on
+    )
+  }
+
   def toJomlMatrix3f: Matrix3f = {
     Matrix3f(
       // format: off
-      c0r0, c0r1, c0r2, // column 1
-      c1r0, c1r1, c1r2, // column 2
-      c2r0, c2r1, c2r2 // column 3
+      c0r0, c0r1, c0r2,  // column 0
+      c1r0, c1r1, c1r2,  // column 1
+      c2r0, c2r1, c2r2   // column 2
       // format: on
     )
   }
@@ -39,9 +58,9 @@ object Matrix3 {
   val IDENTITY: Matrix3 =
     Matrix3(
           // format: off
-          1f, 0f, 0f, // column 1
-          0f, 1f, 0f, // column 2
-          0f, 0f, 1f // column 3
+          1f, 0f, 0f,  // column 0
+          0f, 1f, 0f,  // column 1
+          0f, 0f, 1f   // column 2
           // format: on
     )
 
@@ -51,9 +70,9 @@ object Matrix3 {
     * Example usage:
     * {{{
     * val translationMatrix = Matrix3.translation(1.0f, 2.0f)
-    * val vector = Vector2(10.0f, 10.0f)
+    * val vector = Vector2(10.0f, 5.0f)
     * val translatedVector = translationMatrix * vector
-    * println(translatedVector) // output: Vector2(11.0f, 12.0f)
+    * println(translatedVector) // output: Vector2(11.0f, 7.0f)
     * }}}
     *
     * @param x
@@ -66,9 +85,9 @@ object Matrix3 {
   def translation(x: Float, y: Float): Matrix3 = {
     Matrix3(
       // format: off
-      1f, 0f, 0f, // column 1
-      0f, 1f, 0f, // column 2
-      x, y, 1f // column 3
+      1f, 0f, 0f,  // column 0
+      0f, 1f, 0f,  // column 1
+      x, y, 1f     // column 2
       // format: on
     )
   }
@@ -79,9 +98,9 @@ object Matrix3 {
     * Example usage:
     * {{{
     * val translationMatrix = Matrix3.translation(1.0f, 2.0f)
-    * val vector = Vector2(10.0f, 10.0f)
+    * val vector = Vector2(10.0f, 5.0f)
     * val translatedVector = translationMatrix * vector
-    * println(translatedVector) // output: Vector2(11.0f, 12.0f)
+    * println(translatedVector) // output: Vector2(11.0f, 7.0f)
     * }}}
     *
     * @param delta
@@ -99,10 +118,10 @@ object Matrix3 {
     *
     * Example usage:
     * {{{
-    * val translationMatrix = Matrix3.translation(pi/2f)
-    * val vector = Vector2(10.0f, 10.0f)
+    * val translationMatrix = Matrix3.translation(pi/2f) // clockwise rotation
+    * val vector = Vector2(10.0f, 5.0f)
     * val translatedVector = translationMatrix * vector
-    * println(translatedVector) // output: Vector2(-10.0f, 10.0f)
+    * println(translatedVector) // output: Vector2(5.0f, -10.0f)
     * }}}
     *
     * @param radians
@@ -113,10 +132,69 @@ object Matrix3 {
   def rotation(radians: Float): Matrix3 = {
     Matrix3(
       // format: off
-      cos(radians), -sin(radians), 0f, // row 1
-      sin(radians), cos(radians), 0f, // row 2
-      0f, 0f, 1f // row 3
+      cos(radians), -sin(radians), 0f, // column 0
+      sin(radians), cos(radians), 0f,  // column 1
+      0f, 0f, 1f                       // column 2
       // format: on
     )
+  }
+
+  /** Create a 3x3 matrix which applies a scaling when multiplied with a 2D
+    * vector.
+    *
+    * Example usage:
+    * {{{
+    * val scalingMatrix = Matrix3.scaling(2.0f, 3.0f)
+    * val vector = Vector2(10.0f, 5.0f)
+    * val scaledVector = scalingMatrix * vector
+    * println(scaledVector) // output: Vector2(20.0f, 15.0f)
+    * }}}
+    *
+    * @param x
+    *   The x-component of the scaling.
+    * @param y
+    *   The y-component of the scaling.
+    * @return
+    *   A 3x3 matrix that represents a 2D scaling.
+    */
+  def scaling(x: Float, y: Float): Matrix3 = {
+    Matrix3(
+      // format: off
+      x, 0f, 0f,  // column 0
+      0f, y, 0f,  // column 1
+      0f, 0f, 1f  // column 2
+      // format: on
+    )
+  }
+
+  /** Create a 3x3 matrix which applies a scaling when multiplied with a 2D
+    * vector.
+    *
+    * Example usage:
+    * {{{
+    * val scalingMatrix = Matrix3.scaling(2.0f, 3.0f)
+    * val vector = Vector2(10.0f, 5.0f)
+    * val scaledVector = scalingMatrix * vector
+    * println(scaledVector) // output: Vector2(20.0f, 15.0f)
+    * }}}
+    *
+    * @param scale
+    *   A 2D vector representing the x-component and y-component of the scaling.
+    * @return
+    *   A 3x3 matrix that represents a 2D scaling.
+    */
+  def scaling(scale: Vector2): Matrix3 = {
+    scaling(scale.x, scale.y)
+  }
+
+  def transform(
+      translation: Vector2 = Vector2.zero,
+      rotation: Float = 0,
+      scale: Vector2 = Vector2.one
+  ): Matrix3 = {
+    val rot = Matrix3.rotation(rotation)
+    val sca = Matrix3.scaling(scale)
+    val tra = Matrix3.translation(translation)
+    tra * rot * sca
   }
 }
--- src/test/scala/engine/math/Matrix3Spec.scala
+++ src/test/scala/engine/math/Matrix3Spec.scala
@@ -6,7 +6,7 @@ import engine.test_utils.assertNearEquals
 
 class Matrix3Spec extends AnyFlatSpec with Matchers {
 
-  "A Matrix3" should "multiplying IDENTITY with a Vector2 should not change it" in {
+  "Matrix3" should "multiplying IDENTITY with a Vector2 should not change it" in {
     val v2 = Vector2(3f, 7f)
     assertNearEquals(Matrix3.IDENTITY * v2, v2)
   }
@@ -17,5 +17,37 @@ class Matrix3Spec extends AnyFlatSpec with Matchers {
     val v2 = Vector2(0f, 0f)
     assertNearEquals(t * v2, Vector2(3f, 2f))
     assertNearEquals(t2 * v2, Vector2(3f, 2f))
+  }
+
+  it should "create a proper rotation matrix" in {
+    val r = Matrix3.rotation(pi / 2f)
+    val v2 = Vector2(10f, 5f)
+    assertNearEquals(r * v2, Vector2(5.0f, -10.0f))
+  }
+
+  it should "create a proper scaling matrix" in {
+    val s = Matrix3.scaling(2f, 3f)
+    val s2 = Matrix3.scaling(Vector2(2f, 3f))
+    val v2 = Vector2(10f, 5f)
+    assertNearEquals(s * v2, Vector2(20f, 15f))
+    assertNearEquals(s2 * v2, Vector2(20f, 15f))
+  }
+
+  it should "create a transform matrix with proper translation" in {
+    val transform = Matrix3.transform(translation = Vector2(3f, 2f))
+    val v2 = Vector2(0f, 0f)
+    assertNearEquals(Vector2(3f, 2f), transform * v2)
+  }
+
+  it should "create a transform matrix with proper rotation" in {
+    val transform = Matrix3.transform(rotation = -pi / 2f)
+    val v2 = Vector2(10f, 5f)
+    assertNearEquals(Vector2(-5.0f, 10.0f), transform * v2)
+  }
+
+  it should "create a transform matrix with proper scaling" in {
+    val transform = Matrix3.transform(scale = Vector2(2f, 3f))
+    val v2 = Vector2(10f, 5f)
+    assertNearEquals(Vector2(20f, 15f), transform * v2)
   }
 }
