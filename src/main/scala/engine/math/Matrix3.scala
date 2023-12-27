f283e68caaf830facc1692c346607d694c4b4328
accmltr <johanreynekemail@gmail.com>
2023-12-27 14:28:41 +1300

    In 'math', manually reimplement Matrix3 and Matrix4

--- src/main/scala/engine/math/Matrix3.scala
+++ src/main/scala/engine/math/Matrix3.scala
@@ -1,55 +1,21 @@
 package engine.math
 
+import org.joml.Matrix3f
+
 final case class Matrix3(
-    m00: Float = 1.0f,
-    m01: Float = 0.0f,
-    m02: Float = 0.0f,
-    m10: Float = 0.0f,
-    m11: Float = 1.0f,
-    m12: Float = 0.0f,
-    m20: Float = 0.0f,
-    m21: Float = 0.0f,
-    m22: Float = 1.0f
+    // format: off
+    c0r0: Float, c0r1: Float, c0r2: Float,
+    c1r0: Float, c1r1: Float, c1r2: Float,
+    c2r0: Float, c2r1: Float, c2r2: Float
+    // format: on
 ) {
-  def *(other: Matrix3): Matrix3 = {
-    Matrix3(
-      m00 * other.m00 + m01 * other.m10 + m02 * other.m20,
-      m00 * other.m01 + m01 * other.m11 + m02 * other.m21,
-      m00 * other.m02 + m01 * other.m12 + m02 * other.m22,
-      m10 * other.m00 + m11 * other.m10 + m12 * other.m20,
-      m10 * other.m01 + m11 * other.m11 + m12 * other.m21,
-      m10 * other.m02 + m11 * other.m12 + m12 * other.m22,
-      m20 * other.m00 + m21 * other.m10 + m22 * other.m20,
-      m20 * other.m01 + m21 * other.m11 + m22 * other.m21,
-      m20 * other.m02 + m21 * other.m12 + m22 * other.m22
-    )
-  }
-
-  def *(other: Vector2): Vector2 = {
-    Vector2(
-      m00 * other.x + m01 * other.y + m02,
-      m10 * other.x + m11 * other.y + m12
-    )
-  }
-}
-
-object Matrix3 {
-
-  val IDENTITY = Matrix3()
-
-  def apply(position: Vector2, rotation: Float, scale: Float): Matrix3 = {
-    val cos = Math.cos(rotation).toFloat
-    val sin = Math.sin(rotation).toFloat
-    Matrix3(
-      cos * scale,
-      -sin * scale,
-      position.x,
-      sin * scale,
-      cos * scale,
-      position.y,
-      0.0f,
-      0.0f,
-      1.0f
+  def toJomlMatrix3f: Matrix3f = {
+    Matrix3f(
+      // format: off
+      c0r0, c1r0, c2r0, // row 1
+      c0r1, c1r1, c2r1, // row 2
+      c0r2, c1r2, c2r2 // row 3
+      // format: on
     )
   }
 }
--- src/main/scala/engine/math/Matrix4.scala
+++ src/main/scala/engine/math/Matrix4.scala
@@ -1,35 +1,23 @@
 package engine.math
 
+import org.joml.Matrix4f
+
 final case class Matrix4(
-    m00: Float = 1.0f,
-    m01: Float = 0.0f,
-    m02: Float = 0.0f,
-    m03: Float = 0.0f,
-    m10: Float = 0.0f,
-    m11: Float = 1.0f,
-    m12: Float = 0.0f,
-    m13: Float = 0.0f,
-    m20: Float = 0.0f,
-    m21: Float = 0.0f,
-    m22: Float = 1.0f,
-    m23: Float = 0.0f,
-    m30: Float = 0.0f,
-    m31: Float = 0.0f,
-    m32: Float = 0.0f,
-    m33: Float = 1.0f
+    // format: off
+    c0r0: Float, c0r1: Float, c0r2: Float,c0r3: Float,
+    c1r0: Float, c1r1: Float, c1r2: Float, c1r3: Float,
+    c2r0: Float, c2r1: Float, c2r2: Float, c2r3: Float,
+    c3r0: Float, c3r1: Float, c3r2: Float, c3r3: Float
+    // format: on
 ) {
-  def *(other: Matrix4): Matrix4 = {
-    Matrix4(
-      m00 * other.m00 + m01 * other.m10 + m02 * other.m20 + m03 * other.m30,
-      m00 * other.m01 + m01 * other.m11 + m02 * other.m21 + m03 * other.m31,
-      m00 * other.m02 + m01 * other.m12 + m02 * other.m22 + m03 * other.m32,
-      m00 * other.m03 + m01 * other.m13 + m02 * other.m23 + m03 * other.m33,
-      m10 * other.m00 + m11 * other.m10 + m12 * other.m20 + m13 * other.m30,
-      m10 * other.m01 + m11 * other.m11 + m12 * other.m21 + m13 * other.m31,
-      m10 * other.m02 + m11 * other.m12 + m12 * other.m22 + m13 * other.m32,
-      m10 * other.m03 + m11 * other.m13 + m12 * other.m23 + m13 * other.m33,
-      m20 * other.m00 + m21 * other.m10 + m22 * other.m20 + m23 * other.m30,
-      m20 * other.m01 + m21 * other.m11 + m22 * other.m21 + m23 * other.m31
+  def toJomlMatrix4f: Matrix4f = {
+    Matrix4f(
+      // format: off
+      c0r0, c1r0, c2r0,c3r0, // row 1
+      c0r1, c1r1, c2r1,c3r1, // row 2
+      c0r2, c1r2, c2r2,c3r2, // row 3
+      c0r3, c1r3, c2r3,c3r3 // row 4
+      // format: on
     )
   }
 }
