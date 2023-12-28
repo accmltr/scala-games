944c2fe9cf58e7d96c5078acf3f9e1d0867511a1
accmltr <johanreynekemail@gmail.com>
2023-12-28 22:59:58 +1300

    Fix RenderManager generics

--- src/main/scala/engine/render/render_manager/RenderManager.scala
+++ src/main/scala/engine/render/render_manager/RenderManager.scala
@@ -8,21 +8,21 @@ import org.lwjgl.opengl.GL20._
 import org.lwjgl.opengl.GL30._
 import engine.render.shader.Shader
 
-trait RenderManager {
+trait RenderManager[T] {
 
-  private[render] def +=(element: RenderedElement): Unit
+  private[render] def +=(element: T): Unit
 
-  private[render] def -=(element: RenderedElement): Unit
+  private[render] def -=(element: T): Unit
 
   private[render] def renderLayer(layer: Float): Unit
 }
 
-final case class MeshRenderManager() extends RenderManager {
+final case class MeshRenderManager() extends RenderManager[RenderedMesh] {
 
   private var _layerMap: Map[Float, Map[Shader, List[RenderedMesh]]] = Map.empty
 
-  private[render] override def +=(element: RenderedElement): Unit = {
-    // TODO: Fix this mess
+  private[render] override def +=(element: RenderedMesh): Unit = {
+    // TODO: Fix the generics
     element match {
       case e: RenderedMesh =>
         val shaderMap = _layerMap.getOrElse(e.layer, Map.empty)
@@ -36,7 +36,7 @@ final case class MeshRenderManager() extends RenderManager {
     }
   }
 
-  private[render] override def -=(element: RenderedElement): Unit = {
+  private[render] override def -=(element: RenderedMesh): Unit = {
     // TODO: Fix this mess
     element match {
       case e: RenderedMesh => {
@@ -69,34 +69,43 @@ final case class MeshRenderManager() extends RenderManager {
   }
 
   private[render] override def renderLayer(layer: Float): Unit = {
-    for (e <- _layerMap.filter(_.layer == layer)) {
-      // Create and bind a VAO
-      val vaoId = glGenVertexArrays()
-      glBindVertexArray(vaoId)
 
-      // Create and bind a VBO for the vertices
-      val vboId = glGenBuffers()
-      glBindBuffer(GL_ARRAY_BUFFER, vboId)
-      glBufferData(GL_ARRAY_BUFFER, e.mesh.vertices, GL_STATIC_DRAW)
-      glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0)
-      glEnableVertexAttribArray(0)
+    _layerMap.get(layer) match
+      case None => assert(false, "Cannot render unknown layer")
+      case Some(shaderMap) => {
+        for (shader, elements) <- shaderMap
+        do println(s"shader: $shader   elements: $elements")
+      }
 
-      // Create and bind a VBO for the indices
-      val eboId = glGenBuffers()
-      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboId)
-      glBufferData(GL_ELEMENT_ARRAY_BUFFER, e.mesh.indices, GL_STATIC_DRAW)
+    // {
 
-      // Unbind the VAO
-      glBindVertexArray(0)
+    //   // Create and bind a VAO
+    //   val vaoId = glGenVertexArrays()
+    //   glBindVertexArray(vaoId)
 
-      e.shader.use()
+    //   // Create and bind a VBO for the vertices
+    //   val vboId = glGenBuffers()
+    //   glBindBuffer(GL_ARRAY_BUFFER, vboId)
+    //   glBufferData(GL_ARRAY_BUFFER, e.mesh.vertices, GL_STATIC_DRAW)
+    //   glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0)
+    //   glEnableVertexAttribArray(0)
 
-      // Upload uniforms
-      e.uploadUniforms()
+    //   // Create and bind a VBO for the indices
+    //   val eboId = glGenBuffers()
+    //   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboId)
+    //   glBufferData(GL_ELEMENT_ARRAY_BUFFER, e.mesh.indices, GL_STATIC_DRAW)
 
-      glBindVertexArray(vaoId)
-      glDrawElements(GL_TRIANGLES, e.mesh.indices.limit(), GL_UNSIGNED_INT, 0)
-      glBindVertexArray(0)
-    }
+    //   // Unbind the VAO
+    //   glBindVertexArray(0)
+
+    //   e.shader.use()
+
+    //   // Upload uniforms
+    //   e.uploadUniforms()
+
+    //   glBindVertexArray(vaoId)
+    //   glDrawElements(GL_TRIANGLES, e.mesh.indices.limit(), GL_UNSIGNED_INT, 0)
+    //   glBindVertexArray(0)
+    // }
   }
 }
