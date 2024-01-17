96f0b0a626afcacd318c92b7e3a3ab0a67d69250
accmltr <johanreynekemail@gmail.com>
2024-01-17 23:18:46 +1300

    Update 'shader.ShaderSource' and 'io.package'

--- src/main/scala/engine/render/shader/ShaderSource.scala
+++ src/main/scala/engine/render/shader/ShaderSource.scala
@@ -1,13 +1,23 @@
 package engine.render.shader
 
+/** Stateless read-only class that holds paths to vertex and fragment shader
+  * source files.
+  *
+  * @param vertPath
+  * @param fragPath
+  */
 final case class ShaderSource(
-    val vertPath: String,
-    val fragPath: String
+    val vertexPath: String,
+    val fragmentPath: String
 ) {
+
+  def vertexSource: Option[String] = engine.io.readTextFile(vertexPath)
+  def fragmentSource: Option[String] = engine.io.readTextFile(fragmentPath)
+
   override def equals(x: Any): Boolean =
     x.match
       case ShaderSource(vertPath, fragPath) =>
-        this.vertPath == vertPath && this.fragPath == fragPath
+        this.vertexPath == vertexPath && this.fragmentPath == fragmentPath
       case _ => false
 
 }
