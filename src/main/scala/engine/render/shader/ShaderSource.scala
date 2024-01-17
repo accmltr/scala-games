425d9a888488fe33b463b7b490b7035016fa16f3
accmltr <johanreynekemail@gmail.com>
2024-01-17 23:46:37 +1300

    Add 'ShaderSource.validate'

--- src/main/scala/engine/render/shader/ShaderSource.scala
+++ src/main/scala/engine/render/shader/ShaderSource.scala
@@ -11,6 +11,23 @@ final case class ShaderSource(
     val fragmentPath: String
 ) {
 
+  /** Validates whether the vertex and fragment shader source files exist.
+    * @throws IllegalArgumentException
+    *   If either the vertex or fragment shader source files do not exist.
+    */
+  def validate(): Unit = {
+    if vertexSource.isEmpty
+    then
+      throw new IllegalArgumentException(
+        s"Vertex shader not found: $vertexPath"
+      )
+    if fragmentSource.isEmpty
+    then
+      throw new IllegalArgumentException(
+        s"Fragment shader not found: $fragmentPath"
+      )
+  }
+
   def vertexSource: Option[String] = engine.io.readTextFile(vertexPath)
   def fragmentSource: Option[String] = engine.io.readTextFile(fragmentPath)
 
