2b7cb5d4753c7c216ae167c0210504d462640644
accmltr <johanreynekemail@gmail.com>
2024-03-30 00:35:09 +1300

    Quick save

--- src/main/scala/lib/instance_management/InstanceManager.scala
+++ src/main/scala/lib/instance_management/InstanceManager.scala
@@ -10,46 +10,56 @@ import scala.collection.immutable.Queue
   * destroying game objects in a game engine.
   */
 class InstanceManager[T]() {
-  private var _instances: Map[Int, Ref[T]] = Map.empty
-  private var _nextId: Int = 0
+  private var _refs: Map[Int, T] = Map.empty
+  private var _nextRefNr: Int = 0
   private var _historySize: Int = 10000
   private var _destroyedIds: Queue[Int] = Queue.empty
 
-  def instances: Vector[Ref[T]] = _instances.values.toVector
   def historySize: Int = _historySize
   def historySize_=(value: Int): Unit =
     _historySize = value
     if (_destroyedIds.size > _historySize)
       _destroyedIds = _destroyedIds.drop(_destroyedIds.size - _historySize)
+
   def destroyedIds: Vector[Int] = _destroyedIds.toVector
 
-  def newId: Int = this.synchronized {
-    val id = _nextId
-    _nextId += 1
+  def totalRegistered: Int = _nextRefNr
+  private def _newRefNr(): Int = this.synchronized {
+    val id = _nextRefNr
+    _nextRefNr += 1
     id
   }
+
+  def instance(refNr: Int): Option[T] =
+    Option(_refs(refNr))
+
+  def managedRefNrs: List[Int] =
+    _refs.map(_._1).toList
 
   /** **Note:** Be sure to get rid of local references to the newly registered
     * instance, and use only the returned `Instance` from there on out.
     *
-    * @param rawInstance
-    *   The instance to register and encapsulate within the `Instance` wrapper
-    *   class.
+    * @param t
+    *   The object to be encapsulated.
     * @return
     */
-  def register(rawInstance: T): Ref[T] = this.synchronized {
-    require(rawInstance != null, "'rawInstance' parameter may not be null")
-    val ref = Ref(Option(rawInstance), newId, this)
-    _instances += ref.id -> ref
+  def register(t: T): Ref[T] = this.synchronized {
+    require(t != null, "'t' may not be null")
+    var refNr = _newRefNr()
+    val ref = Ref(refNr, this)
+    _refs += refNr -> t
     ref
   }
 
-  private[instance_management] def _onDestroyed(instance: Ref[T]): Unit =
+  private[instance_management] def destroy(refNr: Int): Unit =
+    require(
+      _refs.contains(refNr),
+      "Trying to destroy non-existant Ref instance."
+    )
     this.synchronized {
-      _instances -= instance.id
+      _refs -= refNr
       if (_destroyedIds.size >= _historySize)
         _destroyedIds = _destroyedIds.dequeue._2
-      _destroyedIds = _destroyedIds.enqueue(instance.id)
+      _destroyedIds = _destroyedIds.enqueue(refNr)
     }
-
 }
--- src/main/scala/lib/instance_management/Ref.scala
+++ src/main/scala/lib/instance_management/Ref.scala
@@ -1,32 +1,13 @@
 package lib.instance_management
 
 final class Ref[T] private[instance_management] (
-    private var _instance: Option[T],
     val id: Int,
     val manager: InstanceManager[T]
 ) {
-  require(
-    _instance.isDefined,
-    "'instance' parameter may not be null"
-  )
-
-  def isDestroyed: Boolean = _instance.isEmpty
-
-  def destroy(): Unit = {
-    _instance = None
-    manager._onDestroyed(this)
-  }
+  def inst: Option[T] = manager.instance(id)
+  def destroy(): Unit = manager.destroy(id)
 }
 
-object Ref {
-  // implicitly convert Reference[T] to T
-  implicit def rti[T](ref: Ref[T]): T = {
-    ref._instance match {
-      case Some(value) => value
-      case None =>
-        throw new IllegalStateException(
-          "Trying to access a destroyed Instance"
-        )
-    }
-  }
-}
+// object Ref {
+//   implicit def rti[T](ref: Ref[T]): T = ref.inst
+// }
