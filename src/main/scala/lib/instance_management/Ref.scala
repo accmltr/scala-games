f3592a3230038d3668f70ff39bd23b072319cbd2
accmltr <johanreynekemail@gmail.com>
2024-03-30 12:56:36 +1300

    Add some sample code to Ref script.

--- src/main/scala/lib/instance_management/Ref.scala
+++ src/main/scala/lib/instance_management/Ref.scala
@@ -15,19 +15,30 @@ object Ref {
   // implicit def rti[T](ref: Ref[T]): T = ref.inst
 }
 
-case class Person(name: String)
+case class Person(name: String, var health: Int = 10) {
+  def damage(amount: Int) =
+    health -= amount
+}
 
 object Test {
   val manager = InstanceManager[Person]()
-  val peter = manager.register(Person("Peter"))
+  val peter = manager.register(Person("Peter", 77))
 
   def main(args: Array[String]): Unit = {
     printPersonRef(peter)
     printPersonRef(Ref[Person](2, manager))
+    peter.match
+      case Ref(p) => p.damage(3)
+
+    printPersonRef(peter)
   }
+
+  def damage(personRef: Ref[Person], amount: Int) =
+    personRef match
+      case Ref(person) => person.health -= amount
 
   def printPersonRef(r: Ref[Person]): Unit =
     r match
-      case Ref(p) => println(p.name)
+      case Ref(p) => println(s"${p.name} has ${p.health} health.")
       case _      => println("No person found in Ref.")
 }
