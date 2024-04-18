d68f0473a5339d4bc6777414bce7a6f085315d96
accmltr <johanreynekemail@gmail.com>
2024-04-19 02:51:26 +1200

    Improve docs for operation_tracking.Op

--- src/main/scala/lib/operation_tracker/Op.scala
+++ src/main/scala/lib/operation_tracker/Op.scala
@@ -20,27 +20,27 @@ final case class Op private[operation_tracker]() {
   val onCompleted: Emitter[Unit] = _onCompletedController.emitter
 
   private[operation_tracker] def start(): Unit =
-    require(state.value == Idle, "Cannot start non-idle operation. [Tip: Use 'resume' to continue operation when paused]")
+    require(state.value == Idle, "Can only 'start' on idle operation. [Tip: Use 'resume' or 'cancel' to leave paused state]")
     _statePropertyController.value = Running
     _onStartedController.emit()
 
   private[operation_tracker] def pause(): Unit =
-    require(state.value == Running, "Cannot pause non-running operation.")
+    require(state.value == Running, "Can only 'pause' on running operation.")
     _statePropertyController.value = Paused
     _onPausedController.emit()
 
   private[operation_tracker] def resume(): Unit =
-    require(state.value == Paused, "Cannot resume non-paused operation.")
+    require(state.value == Paused, "Can only 'resume' on paused operation.")
     _statePropertyController.value = Running
     _onResumedController.emit()
 
   private[operation_tracker] def cancel(): Unit =
-    require(state.value == Running || state.value == Paused, "Cannot cancel idle operation.")
+    require(state.value == Running || state.value == Paused, "Can only 'cancel' on running or paused operation.")
     _statePropertyController.value = Idle
     _onCancelledController.emit()
 
   private[operation_tracker] def complete(): Unit =
-    require(state.value == Running, "Cannot complete non-running operation. [Tip: Resume if paused in order to complete]")
+    require(state.value == Running, "Can only 'complete' on running operation. [Tip: Use 'resume' when paused before using 'complete']")
     _statePropertyController.value = Idle
     _onCompletedController.emit()
 }
